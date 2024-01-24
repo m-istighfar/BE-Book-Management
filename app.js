d5b186f118410d256c255021048affe045db4058
Tifar <istighfar.amal@gmail.com>
2024-01-24 21:47:41 +0700

    feat(swagger): Add OpenAPI YAML for API documentation

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const prisma = require("./config/prisma");
 
 const express = require("express");
 
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yaml");
+const fs = require("fs");
+
 const authMiddleware = require("./middleware/authenticationMiddleware");
 const errorFormatter = require("./middleware/errorFormatter");
 const applyMiddleware = require("./middleware/index");
@@ -16,6 +20,14 @@ const booksRoutes = require("./routes/booksRoutes");
 const app = express();
 applyMiddleware(app);
 
+const openApiPath = "doc/openapi.yaml";
+const file = fs.readFileSync(openApiPath, "utf8");
+const swaggerDocument = yaml.parse(file);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
+});
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/auth", authRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/books", booksRoutes);
